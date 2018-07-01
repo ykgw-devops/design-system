828f2effd03a497e9e9c8cbec9faa76c6aa67925
Gilles De Mey <gilles.de.mey@gmail.com>
2018-07-01 22:44:23 +0200

    format

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -5,10 +5,10 @@ import { cx } from 'react-emotion'
 import { base, kind, outline, pill, disabled, size } from './style'
 
 const Button = props => (
-  <button disabled={props.disabled} className={getStyle(props)}>
-    {props.children}
-  </button>
-)
+    <button disabled={props.disabled} className={getStyle(props)}>
+      {props.children}
+    </button>
+  )
 
 function getStyle (props) {
   return cx(base, kind(props), outline(props), pill(props), disabled(props), size(props))
@@ -25,9 +25,9 @@ Button.propTypes = {
 Button.defaultProps = {
   kind: 'primary',
   size: 'medium',
-  'pill': false,
-  'outline': false,
-  'disabled': false
+  pill: false,
+  outline: false,
+  disabled: false
 }
 
 export default Button
