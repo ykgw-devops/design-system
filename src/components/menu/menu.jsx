4c5a6092ce6ef7d8d836644fa9adcff27668b720
Gilles De Mey <gilles.de.mey@gmail.com>
2018-07-03 23:58:44 +0200

    fix: set nice display name for menu items

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { base, item, active as activeStyle } from './style.jsx'
 import { cx } from '../../emotion'
+import { setDisplayName } from 'recompose'
 
 const Menu = (props) => (
   <ul className={cx(base)}>
@@ -8,12 +9,12 @@ const Menu = (props) => (
   </ul>
 )
 
-Menu.Item = (props) => (
+const Item = (props) => (
   <li className={cx(item, props.active && activeStyle)}>
     {/* wrap child with an anchor tag when "href" prop is truthy */}
-    {props.children.map(child => {
+    {props.children.map((child, i) => {
       return Boolean(props.href)
-        ? wrapAnchor(child, { href: props.href })
+        ? wrapAnchor(child, { href: props.href, key: i })
         : child
     })}
   </li>
@@ -23,5 +24,7 @@ function wrapAnchor (child, props) {
   return <a {...props}>{child}</a>
 }
 
+// we need this so we can have readable component names in the React dev tools
+Menu.Item = setDisplayName('Menu.Item')(Item)
 
 export default Menu
