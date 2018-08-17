2214699d0f07bc4c1886009c938cec3b9385e6ba
Gilles De Mey <gilles.de.mey@gmail.com>
2018-08-17 16:44:40 +0200

    fix(global): add Material Icons to global styles

--- src/global.jsx
+++ src/global.jsx
@@ -7,11 +7,13 @@ injectGlobal`
   }
 
   @import url('https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,600&subset=latin-ext');
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
   @import url('https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css');
 
   body, html {
     -webkit-font-smoothing: antialiased;
     font-size: 14px;
+    font-weight: 400;
   }
 
   body {
