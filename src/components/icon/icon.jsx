825185692c7e4a49feb8c9ddd19e4efdb5e809e1
Gilles De Mey <gilles.de.mey@gmail.com>
2018-08-17 11:49:24 +0200

    chore(icon): pass className

--- src/components/icon/icon.jsx
+++ src/components/icon/icon.jsx
@@ -4,8 +4,12 @@ import PropTypes from 'prop-types'
 import { base } from './style'
 import { cx } from '../../emotion'
 
-const Icon = ({ name }) => {
-  return (<i className={cx(base, 'material-icons')}>{name}</i>)
+const Icon = (props) => {
+  return (
+    <i className={cx(base, 'material-icons', props.className)}>
+      {props.name}
+    </i>
+  )
 }
 
 Icon.propTypes = {
