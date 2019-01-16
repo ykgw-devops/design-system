668a5bd7644d22459de6ca037c746d08ea66fff1
Gilles De Mey <gilles.de.mey@gmail.com>
2019-01-17 00:43:36 +0100

    fix: use react-spring for loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,35 +1,58 @@
 import PropTypes from 'prop-types'
-import React from 'react'
-import { cx } from '../../emotion'
+import React, { useEffect, useState } from 'react'
 import { carbon } from '../../colors'
-import { base } from './loader.styles'
+import { animated, Spring } from 'react-spring'
+
+const from = {
+  transform: 'rotate(0deg)'
+}
+
+const to = {
+  transform: 'rotate(360deg)'
+}
 
 const Loader = (props) => {
-  const { size, color, thickness, gap, speed, className } = props
+  const { size, color, thickness, gap, className, friction, tension } = props
+
+  const [reset, setReset] = useState(false)
+  useEffect(() => {
+    if (!reset) { setReset(true) }
+  }, [reset])
 
   return (
-    <svg
-      height={size}
-      width={size}
-      {...props}
-      className={cx(base, className)}
-      style={{animationDuration: `${speed}ms`}}
-      role='img'
-      aria-labelledby='title desc'
-      viewBox='0 0 32 32'
+    <Spring
+      native
+      config={{ friction, tension }}
+      from={from}
+      to={to}
+      reset={reset}
+      onRest={() => setReset(true)}
     >
-      <circle
-        role='presentation'
-        cx={16}
-        cy={16}
-        r={14 - (thickness / 2)}
-        stroke={color}
-        fill='none'
-        strokeWidth={thickness}
-        strokeDasharray={Math.PI * 2 * (11 - gap)}
-        strokeLinecap='round'
-      />
-    </svg>
+      {style => {
+        return (
+          <animated.svg
+            className={className}
+            height={size}
+            width={size}
+            role='img'
+            viewBox='0 0 32 32'
+            style={style}
+          >
+            <circle
+              role='presentation'
+              cx={16}
+              cy={16}
+              r={14 - (thickness / 2)}
+              stroke={color}
+              fill='none'
+              strokeWidth={thickness}
+              strokeDasharray={Math.PI * 2 * (11 - gap)}
+              strokeLinecap='round'
+            />
+          </animated.svg>
+        )
+      }}
+    </Spring>
   )
 }
 
@@ -46,7 +69,9 @@ Loader.defaultProps = {
   color: carbon,
   thickness: 4,
   gap: 2,
-  speed: 1200
+  speed: 1200,
+  friction: 10,
+  tension: 70
 }
 
 export default Loader
