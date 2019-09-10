e11416dc372999e7289ccbfe48cb4fa78213293a
Gilles De Mey <gilles.de.mey@gmail.com>
2019-09-10 12:06:02 +0200

    feat(timeline): adds timeline component

---
+++ src/components/timeline/Timeline.jsx
@@ -0,0 +1,70 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import styled from '@emotion/styled'
+import { concrete } from '../../Colors'
+
+/**
+ * Timeline
+ *  - Header
+ *  - Entry[]
+ *    - Title
+ *    - Description
+ */
+
+const Timeline = styled.div``
+
+const Header = styled.div``
+
+const Entry = (props) => (
+  <EntryRow>
+    <EntryContent>
+      {props.children}
+    </EntryContent>
+  </EntryRow>
+)
+
+const EntryRow = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const EntryContent = styled.div`
+  border-left: solid 1px ${concrete};
+  padding: 1em;
+  margin-left: 1em;
+  position: relative;
+
+  &:first-of-type {
+    padding-top: 1em;
+  }
+
+  &:before {
+    position: absolute;
+    z-index: 10;
+    display: block;
+    width: 12px;
+    height: 12px;
+    box-sizing: border-box;
+    content: ' ';
+    background-color: ${concrete};
+    border: 2px solid white;
+    border-radius: 6px;
+    left: -0.5em;
+    top: 1.5em;
+  }
+`
+
+const Title = styled.div``
+const Description = styled.div`
+  margin-top: 0.5em;
+  padding: 0.5em 1em;
+  border-radius: 4px;
+  border: solid 1px ${concrete};
+`
+
+Timeline.Header = Header
+Timeline.Entry = Entry
+Timeline.Title = Title
+Timeline.Description = Description
+
+export default Timeline
