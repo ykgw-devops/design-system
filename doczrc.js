17b59634d00b3289a25365014b1e2c64f070870b
Gilles De Mey <gilles.de.mey@gmail.com>
2019-09-24 18:55:51 +0200

    refactor: revert docz v2

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,16 @@
 export default {
   menu: ['Getting started', 'Design', 'Components'],
   title: 'Waylay | Component Library',
-  description: 'Waylay component library'
+  description: 'Waylay component library',
+  indexHtml: 'index.html',
+  hashRouter: true,
+  modifyBabelRc: babelrc => {
+    return {
+      ...babelrc,
+      presets: [
+        ...babelrc.presets,
+        require.resolve('@emotion/babel-preset-css-prop')
+      ]
+    }
+  }
 }
