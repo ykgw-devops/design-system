e5454427df0903bf2c8938ef76d4b4d91326214c
Gilles De Mey <gilles.de.mey@gmail.com>
2019-09-26 15:13:02 +0200

    chore(list): anchor in list fix

--- src/components/list/List.styles.jsx
+++ src/components/list/List.styles.jsx
@@ -2,39 +2,39 @@ import { css } from '@emotion/core'
 import colors, { concrete, clearSky } from '../../Colors'
 import { resetAnchor } from '../../Shared'
 
-const base = css`
-  font-size: 1rem;
-
-  ul {
-    display: inline-block;
-    list-style-type: none;
-    line-height: 1em;
-
-    padding: 0;
-    margin: 0;
-  }
-
-  li {
-    padding: 0.65em 1em;
-    cursor: pointer;
-  }
-`
-
 const active = css`
   color: ${clearSky};
+`
+
+const base = css`
+  font-size: 1rem;
+  line-height: 1.5em;
+
+  ul {
+    margin: 0;
+    padding: 0;
+
+    display: inline-block;
+    list-style-type: none;
+  }
+
+  a {
+    ${resetAnchor};
+  }
+
+  a:hover {
+    ${active};
+  }
 `
 
 const item = css`
   word-wrap: break-word;
   word-break: break-all;
 
-  > a {
-    ${resetAnchor};
-  }
+  display: block;
 
-  > a:hover {
-    ${active};
-  }
+  padding: 0.65em 1em;
+  cursor: pointer;
 `
 
 const interactive = css`
