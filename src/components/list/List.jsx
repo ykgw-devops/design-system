756e4ad8f64575582998dbcb360dbaa9fa212878
Gilles De Mey <gilles.de.mey@gmail.com>
2019-10-22 11:37:43 +0200

    feat: adds compact property for lists

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 import { setDisplayName } from 'recompose'
 
 import { base, item, active as activeStyle, interactive, nonInteractive, divider } from './List.styles.jsx'
 
+const Item = styled.div(props => (
+  [item, props.active && activeStyle, props.nonInteractive && nonInteractive]
+))
+
+const compact = css`
+  ${Item} { padding: 0; }
+`
+
 const ListContainer = styled.div`
   ${base};
   ${props => props.interactive && interactive};
+  ${props => props.compact && compact};
 `
 
 const List = (props) => (
@@ -14,10 +24,6 @@ const List = (props) => (
     {props.children}
   </ListContainer>
 )
-
-const Item = styled.div(props => (
-  [item, props.active && activeStyle, props.nonInteractive && nonInteractive]
-))
 
 const Divider = (props) => (
   <Item {...props} nonInteractive css={divider} />
