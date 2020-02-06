6bd3c8b144ecdf1623c8caedbdbd587177ff8330
lorenzovds <lorenzo.vandesijpe@hotmail.nl>
2020-02-06 13:26:17 +0100

    feat: basic wizard functionality

--- src/components/wizard/Wizard.tsx
+++ src/components/wizard/Wizard.tsx
@@ -1,22 +1,67 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState } from 'react'
+import styled from '@emotion/styled'
+import { get } from 'lodash'
 
-const Collapsible = ({ children = [] }) => {
+import Button from '../button/Button'
+import Icon from '../icon/Icon'
+
+const Controls = styled.div`
+  display: flex;
+`
+
+const Control = styled(Button)`
+  margin-left: 5px;
+`
+
+const Wizard = ({ children = [] }) => {
   const [step, setStep] = useState(0)
-  const totalSteps = children.length
+  const endIndex = children.length - 1
 
   const next = () => {
     const nextStep = step + 1
-    return (nextStep > totalSteps) ? setStep(nextStep) : setStep(step)
+
+    if (nextStep > endIndex) return
+
+    setStep(nextStep)
   }
+
   const previous = () => {
     const prevStep = step - 1
-    return (prevStep > totalSteps) ? setStep(prevStep) : setStep(step)
+
+    if (prevStep < 0) return
+
+    setStep(prevStep)
   }
 
-  return children.map((child) => {
-    const { props } = child
-    return { ...child, props: { ...props, style: { color: 'red' } } }
-  })
+  return (
+    <div>
+      {
+        children.map((child, index) => {
+          const currentStyle = get(child, ['props', 'style'], {})
+          const newStyle = {
+            ...currentStyle,
+            display: index === step ? 'block' : 'none'
+          }
+          const newProps = {
+            ...child.props,
+            style: newStyle
+          }
+          return {
+            ...child,
+            props: newProps
+          }
+        })
+      }
+      <Controls>
+        <Control disabled={step === 0} onClick={previous} >
+          <Icon name='navigate_before' /> Prev
+        </Control>
+        <Control disabled={step === endIndex} onClick={next} >
+          <Icon name='navigate_next' /> Next
+        </Control>
+      </Controls>
+    </div>
+  )
 }
 
-export default Collapsible
+export default Wizard
