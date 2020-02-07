d40c104f88cca59583de800bf67356bd32d568e8
Gilles De Mey <gilles.de.mey@gmail.com>
2020-02-07 12:32:09 +0100

    fix: use JSX pragmas

--- src/components/label/Label.jsx
+++ src/components/label/Label.jsx
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 import styled from '@emotion/styled'
 import isPropValid from '@emotion/is-prop-valid'
 import PropTypes from 'prop-types'
--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 
 import styled from '@emotion/styled'
 import isPropValid from '@emotion/is-prop-valid'
