a1ae58e0d71fc9c798eb1f4713f0b25c68f14c5c
Brent Timmermans <brenttimmermans7@gmail.com>
2020-03-11 17:17:12 +0100

    feat(input): add focus select (#31)

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from '@emotion/styled'
 import isPropValid from '@emotion/is-prop-valid'
 import PropTypes from 'prop-types'
@@ -5,9 +6,15 @@ import PropTypes from 'prop-types'
 import Group from './Group'
 import { input, fluid as fluidStyle, basic as basicStyle, adornments } from './Input.styles'
 
-const Input = styled('input', {
+const handleFocus = e => e.target.select()
+
+const ValidInput = styled('input', {
   shouldForwardProp: isPropValid
-})`
+})``
+
+const BaseInput = props => <ValidInput {...props} onFocus={props.focusSelect && handleFocus} />
+
+const Input = styled(BaseInput)`
   ${input};
   ${props => props.fluid && fluidStyle};
   ${props => props.basic && basicStyle};
